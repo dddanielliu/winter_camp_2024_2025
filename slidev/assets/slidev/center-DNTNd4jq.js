9ce9a8edb7d260501550e317fc451110dfa77186
dddanielliu <dddanielliu@gmail.com>
2024-10-08 10:07:15 +0800

    minor changes and add sidev

---
+++ slidev/assets/slidev/center-DNTNd4jq.js
@@ -0,0 +1,1 @@
+import{o as t,b as o,e as s,f as c}from"../modules/vue-lhAt1M6-.js";import{_ as r}from"../index-DiZkp4qe.js";const a={},n={class:"slidev-layout center h-full grid place-content-center"},l={class:"my-auto"};function d(e,_){return t(),o("div",n,[s("div",l,[c(e.$slots,"default")])])}const m=r(a,[["render",d]]);export{m as I};
