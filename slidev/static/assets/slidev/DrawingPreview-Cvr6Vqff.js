dd397af2e62322efb0bf0d267028f0a82f54b395
dddanielliu <dddanielliu@gmail.com>
2024-10-10 21:42:06 +0800

    update fix

---
+++ slidev/static/assets/slidev/DrawingPreview-Cvr6Vqff.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DrawingPreview.vue_vue_type_script_setup_true_lang-D3JsoKXy.js";import"../modules/vue-BaJc6KuR.js";import"../index-Yk_CVpAl.js";import"../modules/shiki-B0Gxe0S4.js";export{o as default};
